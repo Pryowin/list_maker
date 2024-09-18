2b376efee6e91e85d4d655528df51544215b48e0
David Burke <dburke@amberfire.net>
2024-09-18 00:02:46 -0400

    Minor refactor

--- lists/categories/routes.py
+++ lists/categories/routes.py
@@ -14,16 +14,18 @@ category_api = Blueprint('category_api', __name__)
 @category_api.route('/lists/categories', methods=['GET'])
 @jwt_required(optional=True)
 def read_categories():
+    anon = True
     current_user = get_jwt_identity()
     if current_user:
         user = User.query.filter_by(email=current_user).first()
         if user:
             user_id = user.user_id
-            categories = Category.query.filter((Category.created_by == ADMIN_USER) | (Category.created_by == user_id)).order_by(Category.category_name).all()
-        else:
-            categories = Category.query.filter(Category.created_by == ADMIN_USER ).order_by(Category.category_name).all()    
-    else:
+            anon = False
+        
+    if anon:
         categories = Category.query.filter(Category.created_by == ADMIN_USER).order_by(Category.category_name).all()
+    else:
+        categories = Category.query.filter((Category.created_by == ADMIN_USER) | (Category.created_by == user_id)).order_by(Category.category_name).all()
     
     print(categories)
     schema = CategorySchema(many=True)        
