dfc348c2fdf10dd9d45ca6728382261b1e848634
David Burke <dburke@amberfire.net>
2024-09-25 15:20:46 -0400

    Read all lists functionality added and Pylint related refactoring

--- lists/headers/routes.py
+++ lists/headers/routes.py
@@ -1,13 +1,14 @@
 import os
 
-from flask import Flask,jsonify,request, Blueprint
+from flask import jsonify,request, Blueprint
 from flask_jwt_extended import jwt_required,get_jwt_identity
 from sqlalchemy.orm import joinedload
 
-from application.constants import ADMIN_USER, INCORRECT_DATA, NOT_AUTHORIZED, NOT_FOUND, OK, RECORD_ADDED, RECORD_UPDATED
+from application.constants import INCORRECT_DATA, NOT_AUTHORIZED, NOT_FOUND, OK, RECORD_ADDED
+from application.constants import RECORD_UPDATED
 from application.methods import read_field_from_request
-from lists.headers.validations import is_category_valid, is_list_definition_valid
-from lists.models import ListHeader, ListHeaderSchema, ListItem, header_schema
+from lists.headers.validations import validate_category, validate_definition
+from lists.models import ListHeader, ListItem, header_schema, headers_schema
 from users.models import db, User
 
 header_api = Blueprint('header_api', __name__)
@@ -15,6 +16,7 @@ header_api = Blueprint('header_api', __name__)
 @header_api.route('/lists/headers/create', methods=['POST'])
 @jwt_required()
 def create_header():
+    """Route for creating list header"""
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify(message = "Not authorized to create list."), NOT_AUTHORIZED
@@ -30,30 +32,17 @@ def create_header():
     
     if list_name is None:
         return jsonify(message = "List Name is a required field"), INCORRECT_DATA
+   
+    check = validate_category(list_category, list_created_by)
+    if not check["ok"]:
+        return jsonify(message = check["message"]), check["return_code"]
     
-    if list_category is None:
-        return jsonify(message = "List Category is a required field"), INCORRECT_DATA
-    if list_category.isdigit():
-        list_category_int = int(list_category)
-    else:
-        return jsonify(message = "List Category must be an integer"), INCORRECT_DATA
-    if not is_category_valid(list_created_by, list_category_int):
-        return jsonify(message = "List Category does not exist"), NOT_FOUND
-    
-    
-    if list_definition is None:
-        return jsonify(message = "List Definition is a required field"), INCORRECT_DATA
-    
-    try: 
-        list_definition_dict = eval(list_definition)
-    except:
-        return jsonify(message = "List Definition must be a dictionary"), INCORRECT_DATA
-    
-    if not is_list_definition_valid(list_definition_dict):
-        return jsonify(message = "Invalid List Definition"), INCORRECT_DATA
-    
+    check = validate_definition(list_definition)
+    if not check["ok"]:
+        return jsonify(message = check["message"]), check["return_code"]
+
     header = ListHeader(list_name = list_name, 
-                        list_category = list_category_int, 
+                        list_category = int(list_category),
                         list_created_by = list_created_by,
                         list_definition = list_definition)
     
@@ -62,8 +51,9 @@ def create_header():
     return jsonify(message = "List Header Created"), RECORD_ADDED
 
 @header_api.route('/lists/headers/<int:id>', methods=['GET'])
-@jwt_required()  
+@jwt_required()
 def read_header(id):
+    """Route for reading list header for specified list"""
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify(message = "Not authorized to read list."), NOT_AUTHORIZED
@@ -73,21 +63,38 @@ def read_header(id):
         return jsonify(message = "Not authorized to read list."), NOT_AUTHORIZED
     
     user_id = user.user_id
-    list = ListHeader.query.options(joinedload(ListHeader.category)).filter_by(list_id = id).first()
-    if not list:
+    list_header = ListHeader.query.options(joinedload(ListHeader.category)).filter_by(list_id = id).first()
+    if not list_header:
         return jsonify(message = "List does not exist"), NOT_FOUND
     
-    if user_id != list.list_created_by:
+    if user_id != list_header.list_created_by:
+        return jsonify(message = "Not authorized to read list."), NOT_AUTHORIZED
+        
+    return jsonify(header_schema.dump(list_header)), OK
+
+
+@header_api.route('/lists/headers/', methods=['GET'])
+@jwt_required()  
+def read_headers():
+    """Route for reading all list headers created by current user"""
+    current_user = get_jwt_identity()
+    if not current_user:
+        return jsonify(message = "Not authorized to read list."), NOT_AUTHORIZED
+            
+    user = User.query.filter_by(email=current_user).first()
+    if not user:
         return jsonify(message = "Not authorized to read list."), NOT_AUTHORIZED
     
-    # schema = ListHeaderSchema()
-    # return schema.dump(list)
+    user_id = user.user_id
+    lists = ListHeader.query.options(joinedload(ListHeader.category)).filter_by(list_created_by = user_id).all()
     
-    return jsonify(header_schema.dump(list)), OK
-
+    return jsonify(headers_schema.dump(lists)), OK
+    
+    
 @header_api.route('/lists/headers/update', methods=['PUT'])
-@jwt_required()  
+@jwt_required() 
 def update_header():
+    """Route for updating list header"""
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify(message = "Not authorized to update list."), NOT_AUTHORIZED
@@ -97,49 +104,35 @@ def update_header():
         return jsonify(message = "Not authorized to update list."), NOT_AUTHORIZED
     
     user_id = user.user_id
+    list_category = read_field_from_request(request, 'list_category')
+    check = validate_category(list_category, user_id)
+    if not check["ok"]:
+        return jsonify(message = check["message"]), check["return_code"]
     
-    if list_category is None:
-        return jsonify(message = "List Category is a required field"), INCORRECT_DATA
-    if list_category.isdigit():
-        list_category_int = int(list_category)
-    else:
-        return jsonify(message = "List Category must be an integer"), INCORRECT_DATA
-    if not is_category_valid(user_id, list_category_int):
-        return jsonify(message = "List Category does not exist"), NOT_FOUND
-    
-    list = ListHeader.query.filter_by(list_id = read_field_from_request(request, 'list_id')).first()
-    if not list:
+    list_header = ListHeader.query.filter_by(list_id = read_field_from_request(request, 'list_id')).first()
+    if not list_header:
         return jsonify(message = "List not found"), NOT_FOUND
     
-    if list.created_by != user_id:
+    if list_header.created_by != user_id:
         return jsonify(message = "Not authorized to update list."), NOT_AUTHORIZED
-    
     
     list_id = read_field_from_request(request, 'list_id')
     list_definition = read_field_from_request(request, 'list_definition')
-    if not (list_definition is None):
-        if list_definition != list.list_definition:
-            if ListItem.query_by(list_id=list_id).count() > 0:
-                return jsonify(message = "Unable to change list definition when list items exist."), INCORRECT_DATA
-    try: 
-        list_definition_dict = eval(list_definition)
-    except:
-        return jsonify(message = "List Definition must be a dictionary"), INCORRECT_DATA
-    if not is_list_definition_valid(list_definition_dict):
-        return jsonify(message = "Invalid List Definition"), INCORRECT_DATA
+
+    check = validate_definition(list_definition)
+    if not check["ok"]:
+        return jsonify(message = check["message"]), check["return_code"]
+      
+    if list_definition != list_header.list_definition:
+        if ListItem.query_by(list_id=list_id).count() > 0:
+            return jsonify(message = "Unable to change list definition when list items exist."), INCORRECT_DATA
     
     list_name = read_field_from_request(request, 'list_name')
-    list_category = read_field_from_request(request, 'list_category')
     
-    list.list_definition = list_definition
-    list.list_name = list_name
-    list.list_category = list_category
-    db.session.update(list)
-    db.commit()
+    list_header.list_definition = list_definition
+    list_header.list_name = list_name
+    list_header.list_category = list_category
+    
+    db.session.commit()
     return jsonify(message = 'List Header Updated'),RECORD_UPDATED
-    
-        
-    
-    
-    
 
--- lists/headers/validations.py
+++ lists/headers/validations.py
@@ -1,28 +1,33 @@
-from application.constants import ADMIN_USER, VALID_TYPES
+from ast import literal_eval
+
+from application.constants import (ADMIN_USER, INCORRECT_DATA, NOT_FOUND,
+                                   VALID_TYPES)
 from lists.models import Category
 
 
 def is_list_definition_valid(list_definition: dict) -> bool:
-    
+    """Returns false if any of the column definitions are invalid"""
+
     is_string_present = False
     
     if list_definition == {}:
         return False
     
     is_valid = True
-    for type in list_definition.values():
-        if type not in VALID_TYPES:
+    for data_typ in list_definition.values():
+        if data_typ not in VALID_TYPES:
             is_valid = False
         else:
             if type == "String":
                 is_string_present = True
             
     if not is_string_present:
-       is_valid = False
+        is_valid = False
         
     return is_valid
 
 def is_category_valid(user_id: int, category_id: int) -> bool:
+    """Returns true if the category exists and it is either a default one created by admin user, or if it is created by the current user"""
     category = Category.query.filter_by(category_id=category_id).first()
     
     if not category:
@@ -32,5 +37,34 @@ def is_category_valid(user_id: int, category_id: int) -> bool:
         return True
     
     return False
+
+def validate_category(list_category, list_created_by):
+    """List category must exist, be an integer, and be available to the current user"""
+    if list_category is None:
+        return {"ok": False, "message": "List Category is a required field", "return_code": INCORRECT_DATA}
+        
+    if list_category.isdigit():
+        list_category_int = int(list_category)
+    else:
+        return {"ok": False, "message": "List Category must be an integer", "return_code": INCORRECT_DATA}
+    if not is_category_valid(list_created_by, list_category_int):
+        return {"ok": False, "message": "List Category does not exist", "return_code": NOT_FOUND}
+   
+    return {"ok": True}
+
+def validate_definition(list_definition):
+    """Returns true if list definition is a dictionary with valid values for all keys"""
+    if list_definition is None:
+        return {"ok": False, "message": "List Definition is a required field", "return_code": INCORRECT_DATA}
+       
+    try: 
+        list_definition_dict = literal_eval(list_definition)
+    except ValueError:
+        return {"ok": False, "message": "List Definition must be a dictionary", "return_code": INCORRECT_DATA}
+    
+    if not is_list_definition_valid(list_definition_dict):
+        return {"ok": False, "message": "Invalid List Definition", "return_code": INCORRECT_DATA}
+    
+    return {"ok": True}
 
     
